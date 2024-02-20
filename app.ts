244a150e6b27f0f35435ed88e08a0189864cfb4a
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-02-20 13:46:23 +0100

    Write app logic

--- app.ts
+++ app.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import indexRouter from './routes/index';
+import userRouter from './routes/user';
+import errorHandler from './middlewares/errorHandler';
+import createError from 'http-errors';
 
 const app = express();
 
-app.use('/', (req, res) => {
-  res.send('Status Active!');
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// set routers
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+app.use((req, res, next) => next(createError(500)));
+app.use(errorHandler);
 
 export default app;
