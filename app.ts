edbf442de8dfd1967308d94fde3347c68126aaf0
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-03-01 02:29:22 +0100

    Allow localhost credentials

--- app.ts
+++ app.ts
@@ -6,12 +6,20 @@ import createError from 'http-errors';
 import jpCardRouter from './routes/jp-card';
 import cors from 'cors';
 import loginRouter from './routes/login';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://127.0.0.1:5713', 'http://localhost:5173'],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // set routers
 app.use('/', indexRouter);
